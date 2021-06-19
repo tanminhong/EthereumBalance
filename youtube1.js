2a52c0cb243c10b950b8448cdcc159545b540c0a
tanminhong <79739770+tanminhong@users.noreply.github.com>
2021-06-19 16:46:53 +0800

    Add files via upload

---
+++ youtube1.js
@@ -0,0 +1,41 @@
+if (typeof window.ethereum !=='undefined') {
+console.log('metamask is installed');
+}else{
+console.log('install metamask')
+document.querySelector('.enableEthereumButton').innerHTML="<a href='https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn' target='_blank'> Install Metamask</a>";
+document.querySelector('.intro').style.display='none'
+}
+
+
+const ethereumButton=document.querySelector('.enableEthereumButton');
+const showAccount= document.querySelector('.showAccount');
+const sendEthButton = document.querySelector('.sendEthButton');
+const showBalance = document.querySelector('.showBalance')
+ethereumButton.addEventListener('click',()=> {
+    //will start the metamask extension
+getAccount();
+}); 
+
+async function getAccount(){
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts'});
+    const accounts = accounts[0];
+    showAccount.InnerHTML=account;
+   
+
+    const balance = await ethereum
+    .request ({
+    method:'eth_getBalance',
+    params:[account,"latest"],
+    });
+  
+    const read = parseInt(balance)/10**18;
+    console.log(read.toFixed(5));
+    showBalance.innerHTML=read.toFixed(5);
+    
+    //const value=200;
+    //console.log(value)
+    }       
+
+
+
+    
